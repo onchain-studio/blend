9e1d40f855f10d428e8836b715f68c620f7daa55
andy8052 <andychorlian@gmail.com>
2023-06-28 14:28:02 -0400

    init

---
+++ src/Staking.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "./utils/Errors.sol";
+import "./utils/Structs.sol";
+
+import {IERC20} from "./interfaces/IERC20.sol";
+import {Ownable} from "./utils/Ownable.sol";
+
+contract Staking {
+    
+}
