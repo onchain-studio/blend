592d0a99ee392a1650fe06cc1fce74156b064ed3
andy8052 <andychorlian@gmail.com>
2023-06-29 08:59:16 -0400

    add beedle token and update script

--- script/LenderScript.s.sol
+++ script/LenderScript.s.sol
@@ -3,12 +3,21 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "../src/Lender.sol";
+import "../src/Staking.sol";
+import "../src/Beedle.sol";
+
+import {WETH} from "solady/src/tokens/WETH.sol";
+import {VestingWallet} from "openzeppelin-contracts/contracts/finance/VestingWallet.sol";
 
 contract LenderScript is Script {
     function run() public {
         uint256 deployer = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployer);
-        Lender lender = new Lender();
+        new Lender();
+        address beedle = address(new Beedle());
+        address weth = address(new WETH());
+        address staking = address(new Staking(beedle, weth));
+
         vm.stopBroadcast();
     }
 }
